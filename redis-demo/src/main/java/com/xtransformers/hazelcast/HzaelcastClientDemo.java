991317dff48283f51a60ded42430128b11e34645
hellboy0621 <hero0621@126.com>
2021-01-11 23:35:45 +0800

    [redis-demo]Hazelcast Demo

---
+++ redis-demo/src/main/java/com/xtransformers/hazelcast/HzaelcastClientDemo.java
@@ -0,0 +1,21 @@
+package com.xtransformers.hazelcast;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+
+public class HzaelcastClientDemo {
+
+    public static void main(String[] args) {
+        ClientConfig clientConfig = new ClientConfig();
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+
+        IMap map = client.getMap("customers");
+        System.out.println("Map Size:" + map.size());
+        map.values().forEach(System.out::println);
+
+        client.shutdown();
+    }
+
+}
