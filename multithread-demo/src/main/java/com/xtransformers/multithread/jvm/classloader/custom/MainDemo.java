cd2f85a8a5a1823719896e591488d9a27c67a3b8
hellboy0621 <hero0621@126.com>
2021-12-07 00:25:01 +0800

    feat: [multithread-demo]绕过系统类加载器

--- multithread-demo/src/main/java/com/xtransformers/multithread/jvm/classloader/custom/MainDemo.java
+++ multithread-demo/src/main/java/com/xtransformers/multithread/jvm/classloader/custom/MainDemo.java
@@ -11,6 +11,13 @@ public class MainDemo {
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
             IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+//        test1();
+//        test2();
+        test3();
+    }
+
+    private static void test1() throws ClassNotFoundException, InstantiationException, IllegalAccessException,
+            NoSuchMethodException, InvocationTargetException {
         CustomClassLoader classLoader = new CustomClassLoader("/Users/daniel/Documents/devTools/tmp");
         Class<?> aClass = classLoader.loadClass("com.xtransformers.multithread.jvm.classloader.custom.HelloWorld");
         System.out.println(aClass.getClassLoader());
@@ -24,9 +31,45 @@ public class MainDemo {
         System.out.println(result);
     }
     /**
+     * 把IDEA中的代码删掉
      * Custom ClassLoader
      * Hello World Class is Initialized.
      * com.xtransformers.multithread.jvm.classloader.custom.HelloWorld@3764951d
      * Hello World
      */
+    /**
+     * IDEA 中代码不删掉
+     * sun.misc.Launcher$AppClassLoader@18b4aac2
+     * Hello World Class is Initialized.
+     * com.xtransformers.multithread.jvm.classloader.custom.HelloWorld@d716361
+     * Hello World
+     */
+
+    /**
+     * 不删除IDEA中代码，通过编码绕过系统类加载器
+     *
+     * @throws ClassNotFoundException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     */
+    private static void test2() throws ClassNotFoundException, InstantiationException, IllegalAccessException,
+            NoSuchMethodException, InvocationTargetException {
+        // 1. 直接将扩展类加载器作为自定义类加载器的父加载器
+        ClassLoader extClassLoader = MainDemo.class.getClassLoader().getParent();
+        CustomClassLoader classLoader = new CustomClassLoader(extClassLoader, "/Users/daniel/Documents/devTools/tmp");
+        Class<?> aClass = classLoader.loadClass("com.xtransformers.multithread.jvm.classloader.custom.HelloWorld");
+        // Custom ClassLoader
+        System.out.println(aClass.getClassLoader());
+    }
+
+    private static void test3() throws ClassNotFoundException, InstantiationException, IllegalAccessException,
+            NoSuchMethodException, InvocationTargetException {
+        // 2. 指定自定义类加载器的父加载器为 null
+        CustomClassLoader classLoader = new CustomClassLoader(null, "/Users/daniel/Documents/devTools/tmp");
+        Class<?> aClass = classLoader.loadClass("com.xtransformers.multithread.jvm.classloader.custom.HelloWorld");
+        // Custom ClassLoader
+        System.out.println(aClass.getClassLoader());
+    }
 }
